25f2d63bc79213c91954d8aa0841594aa7b87262
Christian Simmons <csimmonscs@gmail.com>
2019-12-09 08:10:22 -0800

    Uploading GridMap to Github

---
+++ GridMap/Assets/Scripts/Testing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    private Grid<bool> grid;
+
+    private void Start()
+    {
+        grid = new Grid<bool>(20, 10, 6f, Vector3.zero);
+    }
+
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        //}
+
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        //}
+    }
+}
